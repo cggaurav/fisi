4fbdf2688da3d0ba0bc9f25e7ec791331816221c
Gaurav Chandrashekar <cggaurav@gmail.com>
2013-10-18 20:53:23 +0200

    Love animate.css

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 $(function(){
 
 	// console.log("Hello!");
+	$.fn.textWidth = function(text, font) {
+	    if (!$.fn.textWidth.fakeEl) $.fn.textWidth.fakeEl = $('<span>').hide().appendTo(document.body);
+	    $.fn.textWidth.fakeEl.html(text || this.val() || this.text()).css('font', font || this.css('font'));
+	    return $.fn.textWidth.fakeEl.width();
+	};
+
+
+	
+	$('.taskinput').keypress(function (e) {
+	  if (e.which === 13) {
+	  	console.log("FUCK");
+	  	e.preventDefault();
+	    window.location.href = "fisi.html";
+	  }
+	});
 
 });
 	
